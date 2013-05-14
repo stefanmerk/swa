f4e9a855528704711093fbbe22a1ea8bd9aaae26
Julia Thamasett <thju1011@hs-karlsruhe.de>
2013-05-14 15:27:42 +0200

    .xml jsf

--- shop/src/main/java/de/shop/util/Resources.java
+++ shop/src/main/java/de/shop/util/Resources.java
@@ -33,10 +33,10 @@ public class Resources {
       return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
    }
    
-   @Produces
-   @RequestScoped
-   public FacesContext produceFacesContext() {
-      return FacesContext.getCurrentInstance();
-   }
+//   @Produces
+//   @RequestScoped
+//   public FacesContext produceFacesContext() {
+//      return FacesContext.getCurrentInstance();
+//   }
    
 }
